9186ae9c34c29408b69a582734db719ca1e54249
mosavy <www.mortezamosavy94@gmail.com>
2017-01-12 12:45:11 +0330

    Update lock.lua

--- plugins/lock.lua
+++ plugins/lock.lua
@@ -724,15 +724,9 @@ elseif matches[2] == 'video' then
 unmute_video_group(msg, msg.chat_id)
 end
 end
-if matches[1] == 'setlink' and is_owner(msg) or is_momod(msg) then
-redis:set('link'..msg.chat_id_,matches[2])
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link Saved</b>', 1, 'html')
-end					
+			
 end
-if matches[1] == 'setlink' and is_owner(msg) or is_momod(msg) then
-redis:set('link'..msg.chat_id_,matches[2])
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Link Saved</b>', 1, 'html')
-end			
+	
 end
 end
 return {
@@ -743,7 +737,7 @@ return {
 	"^[/#!](unmute) (.*)$",
 		"^[/#!](id)$",
 		"^[/#!](settings)$",
-		"^[/#!](setlink) (.*)$",
+		
 "^!!!edit:[/#!](lock) (.*)$",
 "^!!!edit:[/#!](unlock) (.*)$",
 "^!!!edit:[/#!](mute) (.*)$",
