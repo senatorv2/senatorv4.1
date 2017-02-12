cc77c117346f6853d31a715560f60f6ede099edf
senatorv2 <shpan@hi2.in>
2017-02-13 03:21:16 +0330

    Update setrules.lua

--- plugins/setrules.lua
+++ plugins/setrules.lua
@@ -3,7 +3,7 @@ local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id)]
 if matches[1] == 'setrules' and is_owner(msg) or is_momod(msg) and groupa then
 redis:set('rules'..msg.chat_id_,matches[2])
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Rules Saved</b>', 1, 'html')
+tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Rules Saved</b>\n*Channel:* @senator_tea', 1, 'html')
 end			
 end
 return {
