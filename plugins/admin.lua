602ab4019445e9093bc109923579d61036feec53
senatorv2 <shpan@hi2.in>
2017-02-13 15:30:48 +0330

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -1,14 +1,14 @@
-﻿local function run(msg, matches)
+local function run(msg, matches)
 if is_sudo then
 if matches[1] == 'import' then
 tg.importChatInviteLink('https://telegram.me/joinchat/'..matches[2])
 elseif matches[1] == 'setbotname' then
 if matches[3] then
-text = "Bot name to "..matches[2]..matches[3].." changed"
+text = "نام ربات به "..matches[2]..matches[3].." تغییر یافت."
 tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
 tg.changeName(matches[2], matches[3])
 else
-text = "Bot name to "..matches[2].." changed"
+text = "نام ربات به "..matches[2].." تغییر یافت."
 tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
 tg.changeName(matches[2], '')
 end
@@ -17,7 +17,7 @@ tg.changeUsername(matches[2])
 elseif matches[1] == 'rembotusername' then
 tg.changeUsername('')
 elseif matches[1] == 'setbotphoto' then
-tg.sendMessage(msg.chat_id_, 0, 1, 'Please send the new photo', 1, 'html')
+tg.sendMessage(msg.chat_id_, 0, 1, 'لطفا تصویر را ارسال کنید.', 1, 'html')
 redis:set('botphoto','yes')
 elseif matches[1] == 'photo' and redis:get('botphoto') then
 local file = 'bot/photos/bot.jpg'
@@ -29,11 +29,11 @@ redis:del('botphoto',true)
 end
 if matches[1] == 'markread' then
 if matches[2] == 'on' then
-text = '<b> Mark Read = on</b>'
+text = '<b> Mark Read => on</b>'
 	tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
 redis:set('markread','on')
 elseif matches[2] == 'off' then
-text = '<b> Mark Read = off</b>'
+text = '<b> Mark Read => off</b>'
 	tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
 redis:set('markread','off')
 end
